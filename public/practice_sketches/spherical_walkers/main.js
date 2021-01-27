70e6b604a7a3eb403f3a820268770bd58f3b0f06
kcarollee <kcarollee31693@gmail.com>
2021-01-27 10:51:25 +0000

    perlin walkers on spheres

--- public/practice_sketches/spherical_walkers/main.js
+++ public/practice_sketches/spherical_walkers/main.js
@@ -1,54 +1,79 @@
 import * as THREE from "https://cdn.jsdelivr.net/npm/three@v0.124.0/build/three.module.js";
-import {
-    OrbitControls
-} from "https://cdn.jsdelivr.net/npm/three@v0.124.0/examples/jsm/controls/OrbitControls.js";
-
+import {OrbitControls} from "https://cdn.jsdelivr.net/npm/three@v0.124.0/examples/jsm/controls/OrbitControls.js";
+import {SceneUtils} from "https://cdn.jsdelivr.net/npm/three@0.124.0/examples/jsm/utils/SceneUtils.js";
 class Walker {
     constructor(trailNum, movementRadius, scene) {
         this.trailNum = trailNum;
-        this.deg = 0;
-        this.degInc = 0.1;
-        this.noiseRadius;
+        
+        this.lineMat = new THREE.LineBasicMaterial({
+            color: 0xffffff * Math.random(),
+            linewidth: 5
+        });
+
+        this.colorMat = new THREE.MeshBasicMaterial({
+            color: 0xffffff * Math.random(),
+        });
+        
+        this.movementRadius = movementRadius;
         this.sphereArr = [];
-        this.noiseInc = 0.03;
+
         this.startRand = Math.random() * 100.0;
+        this.noiseStep = 0.01;
+        this.pointArr = [];
+
+        var randDeg1, randDeg2;
         for (let i = 0; i < this.trailNum; i++) {
-            var sphereGeo = new THREE.SphereGeometry(0.5, 10, 10);
-            var sphereMat = new THREE.MeshBasicMaterial({
-                color: 0xffffff
-            });
-            var sphere = new THREE.Mesh(sphereGeo, sphereMat);
-            this.noiseRadius = 10 * noise.simplex2(this.startRand + i * this.noiseInc, 0) + 10;
+            var sphereGeo = new THREE.SphereGeometry(0.1, 5, 5);
+            var sphere = new SceneUtils.createMultiMaterialObject(sphereGeo, [this.colorMat]);
+            randDeg1 = noise.simplex2(this.noiseStep * i, 0) * Math.PI * 2;
+            randDeg2 = noise.simplex2(this.startRand + this.noiseStep * i, 0) * Math.PI * 2;
+            var pos = this.getPos(randDeg1, randDeg2);
             
-            sphere.position.set(this.noiseRadius * Math.cos(this.startRand + this.deg), 
-                this.noiseRadius * Math.sin(this.startRand + this.deg), 0);
-            this.deg += this.degInc;
+            sphere.position.set(pos[0], pos[1], pos[2]);
+            
             scene.add(sphere);
             this.sphereArr.push(sphere);
+
+            var point = new THREE.Vector3(pos[0], pos[1], pos[2]);
+            this.pointArr.push(point);
         }
-        this.movementRadius = movementRadius;
+
+        this.lineGeo = new THREE.BufferGeometry().setFromPoints(this.pointArr);
+        this.line =  new THREE.Line(this.lineGeo, this.lineMat);
+        scene.add(this.line);
         this.positionVec3Arr = [];
     }
 
+    getPos(deg1, deg2){
+        var x = this.movementRadius * Math.cos(deg1) * Math.cos(deg2);
+        var y = this.movementRadius * Math.sin(deg1) * Math.cos(deg2);
+        var z = this.movementRadius * Math.sin(deg2);
+        
+        return [x, y, z];
+    }
+
     update(step) {
-        this.deg = 0;
-        var noiseInc = 0.01;
-        for (let i = 0; i < this.sphereArr.length; i++){
-            this.noiseRadius = 10 * noise.simplex2(this.startRand + i * this.noiseInc + step, 0) + 10;
-            this.sphereArr[i].position.set(this.noiseRadius * Math.cos(this.startRand + this.deg + step), 
-                this.noiseRadius * Math.sin(this.startRand + this.deg + step), this.noiseRadius - 10);
-            this.deg += this.degInc;
+        step *= 0.1;
+        var randDeg1, randDeg2;
+        for (let i = 0; i < this.sphereArr.length; i++) {
+            randDeg1 = noise.simplex2(this.noiseStep * i + step, this.startRand) * Math.PI * 2;
+            randDeg2 = noise.simplex2(this.startRand + this.noiseStep * i + step, this.startRand) * Math.PI * 2;
+            var pos = this.getPos(randDeg1, randDeg2);
+            this.sphereArr[i].position.set(pos[0], pos[1], pos[2]);
+            this.pointArr[i].set(pos[0], pos[1], pos[2]);
+            
         }
+        this.lineGeo.setFromPoints(this.pointArr);
+        
     }
 }
-Walker.mat = new THREE.MeshBasicMaterial({
-    color: 0xcccccc
-});
+
+Walker.depthMat = new THREE.MeshDepthMaterial();
 
 function init() {
     noise.seed(Math.random());
     var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+    var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 10, 130);
     var stats = initStats();
     var renderer = new THREE.WebGLRenderer({
         antialias: true
@@ -57,9 +82,9 @@ function init() {
 
     var walkerArr = [];
 
-    var walkerNum = 30;
+    var walkerNum = 10;
     for (let i = 0; i < walkerNum; i++){
-        var w = new Walker(10, 10, scene);
+        var w = new Walker(10,  (i + 1) * 2, scene);
         walkerArr.push(w);
     }
 
