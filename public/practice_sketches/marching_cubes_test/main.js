2ccfce94e1da674726df2165a46d82d465e2fabe
kcarollee <kcarollee31693@gmail.com>
2021-04-27 23:16:58 +0900

    getting ready for cube marching

--- public/practice_sketches/marching_cubes_test/main.js
+++ public/practice_sketches/marching_cubes_test/main.js
@@ -1,3 +1,4 @@
+import {OrbitControls} from "https://cdn.jsdelivr.net/npm/three@v0.124.0/examples/jsm/controls/OrbitControls.js";
 function main(){
 	const canvas = document.querySelector('#c');
 	const renderer = new THREE.WebGLRenderer({canvas});
@@ -9,13 +10,54 @@ function main(){
 	const far = 1000;
 	const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 
-	camera.position.set(0, 0, 20);
+	camera.position.set(0, 0, 30);
 
 	const scene = new THREE.Scene();
 	scene.background = new THREE.Color(0xCCCCCC);
 	renderer.render(scene, camera);
 
-	
+	const orbitControls = new OrbitControls(camera, renderer.domElement);
+    orbitControls.target.copy(scene.position);
+    orbitControls.update();
+
+// TEST SHAPE FUNCTIONS
+	const f1 = (x, y, z) =>{
+		let val = 10000.0 / 
+			(
+				Math.pow(x, 2) +
+				Math.pow(y, 2) + 
+				Math.pow(z, 2)
+			);
+		return  val > 999 ? 1 : val;
+	} 
+// TEST SPACE
+	let testSpace = {
+		width: 10,
+		height: 10,
+		depth: 10
+	}
+// SINGLE CUBE PARAMS
+	let singleCubeParams = {
+		width: 1,
+		height: 1,
+		depth: 1
+	}
+// DEBUG SPRITES
+
+	for (let w = -testSpace.width * 0.5; w < testSpace.width * 0.5; w += singleCubeParams.width){
+		for (let h =  testSpace.height * -0.5; h < testSpace.height * 0.5; h += singleCubeParams.height){
+			for (let d = -testSpace.depth * 0.5; d < testSpace.depth * 0.5; d += singleCubeParams.depth){
+				const s = f1(w, h, d) * 0.0005;
+				const spriteMat = new THREE.SpriteMaterial({color: new THREE.Color(s, s, s)});
+				const sprite = new THREE.Sprite(spriteMat);
+
+				sprite.position.set(w, h, d);
+				sprite.scale.set(s, s, s);
+				scene.add(sprite);
+			}
+		}
+	}
+
 //GUI
 	const gui = new dat.GUI();
 	const controls = new function(){
