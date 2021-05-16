ea5f97bc7b3f6cdab4b3a5d934cc9786d62111f8
kcarollee <kcarollee31693@gmail.com>
2021-05-16 22:55:29 +0900

    figured out how to modify uvs

--- public/practice_sketches/marching_cubes_test_5_terrains_postprocessed/custom-shader.js
+++ public/practice_sketches/marching_cubes_test_5_terrains_postprocessed/custom-shader.js
@@ -42,22 +42,36 @@ THREE.CustomShader = {
         // used to determine the correct texel we're working on
         "varying vec2 vUv;",
 
+        "float greyBasedVal(vec2 uv, vec2 uv2, float d){",
+            "float v;",
+            "return v;",
+        "}",
+
+        "float circle(vec2 uv,float gval){",
+            "return 1.0 - step(gval, length(uv - vec2(0.5, 0.5)));",
+        "}",
+
         // executed, in parallel, for each pixel
         "void main() {",
 
         
 
         "vec2 uvo = vUv;", // original uv coords
+        "vec3 outCol = vec3(.0);",
         "float m = 0.1;", // pixelate coef
-        "float divNum = 75.0;",
-        "float windowDiv = 1.0 / divNum;",
-        "uvo.x = float(floor(uvo.x / windowDiv)) * windowDiv;",
-        "uvo.y = float(floor(uvo.y / windowDiv)) * windowDiv;",
+        "float divNum = 200.0;",
+        "float gridDim = 1.0 / divNum;",
+        "uvo.x = float(floor(uvo.x * divNum)) * gridDim;",
+        "uvo.y = float(floor(uvo.y * divNum)) * gridDim;",
 
         // get the pixel from the texture we're working with (called a texel)
         "vec4 texel = texture2D( tDiffuse, uvo );",
+
+        "float greyVal = texel.r;",
+        "vec2 uvm = (vUv - uvo) * divNum;", // modified uv coord.
+        "outCol += circle(uvm, greyVal);",
         // return this new color
-        "gl_FragColor = vec4( texel.rgb, 1.0 );",
+        "gl_FragColor = vec4( outCol, 1.0 );",
 
         "}"
 
