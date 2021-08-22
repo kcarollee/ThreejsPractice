a005236461d9116a229eaa4cd46339267b8f2c44
kcarollee <kcarollee31693@gmail.com>
2021-08-22 19:41:20 +0900

    somewhat done with psuedo datamoshing

--- public/practice_sketches/quick_p5_videomosh_test/sketch.js
+++ public/practice_sketches/quick_p5_videomosh_test/sketch.js
@@ -1,15 +1,33 @@
 let video;
+let videoCopy;
+let previousFrame;
 let videoResized = false;
 let indicesNeedingWarp = [];
 let auxPixelArr = [];
 let indicesToWarp = [];
-
+let videoPixelsPrev = [];
+let threshold = 1;
+let shiftCount = 0;
+let swapModeEnabled = false;
 let chooseIndicesToWarp = false;
+
+let assimilateCount = 0; // first source assimilating into the second one
+let assimilate = false;
+
+function mapLinear(x, a1, a2, b1, b2){
+    return b1 + ( x - a1 ) * ( b2 - b1 ) / ( a2 - a1 );
+}
+
+function mix(x, y, a){
+  return x * (1 - a) + y * a;
+}
+
 function setup() {
   createCanvas(400, 400);
-  video = createVideo('test1.mp4');
- 
+  video = createVideo('test3.mp4');
+  videoCopy = createVideo('test2.mp4');
   video.loop();
+  videoCopy.loop();
 }
 
 function draw() {
@@ -30,19 +48,20 @@ function draw() {
     }
   }
 
-
+  
   video.loadPixels();
-
+  videoCopy.loadPixels();
   if (chooseIndicesToWarp) fillIndicesToWarp();
   //if (frameCount % 100 == 0) fillIndicesToWarp();
   for (let y = 0; y < height; y++){
     for (let x = 0; x < width; x++){
       const index = y * width + x;
       const offset = 4 * index;
-      //video.pixels[offset] = 155;
-
+      
+      
       // if the pixel at the current index needs warping
-      if (indicesNeedingWarp[index] == 1){
+      if (indicesNeedingWarp[index] == 1 && !swapModeEnabled){
+        
         // upon first random selection of the pixel, 
         // copy current frame's color values to auxPixelArr
         // use the alpha channel to determine whether this pixel has been analyzed
@@ -62,15 +81,62 @@ function draw() {
           video.pixels[offset + 3] = auxPixelArr[offset + 3];
         }
       }
+
+      // swapMode: comparing with the previous frame.
+      // determining whether a pixel should be warped depending on its gscale value
+      if (swapModeEnabled){
+        let colorSumCur = videoCopy.pixels[offset] + videoCopy.pixels[offset + 1] +
+                       videoCopy.pixels[offset + 2];
+        let colorSumPrev = videoPixelsPrev[offset] + videoPixelsPrev[offset + 1] +
+                       videoPixelsPrev[offset + 2];
+        colorSumCur /= 3;
+        colorSumPrev /= 3;
+        //if (index == 5000) console.log(colorSumCur, colorSumPrev);
+
+        
+        // if the difference is larger than the threshold, set pixel color to aux
+        let greyDiff = Math.abs(colorSumCur - colorSumPrev);
+        if (greyDiff > threshold){ 
+          // have the pixels that are larger than the threshold
+          // have shifted color values to SOMEWHAT simulate datamoshing.
+          let offset2 = (offset + shiftCount) % auxPixelArr.length; 
+          video.pixels[offset] = auxPixelArr[offset2];
+          video.pixels[offset + 1] = auxPixelArr[offset2 + 1];
+          video.pixels[offset + 2] = auxPixelArr[offset2 + 2];
+          video.pixels[offset + 3] = auxPixelArr[offset2 + 3];
+          
+          auxPixelArr[offset] = videoCopy.pixels[offset];
+          auxPixelArr[offset + 1] = videoCopy.pixels[offset + 1];
+          auxPixelArr[offset + 2] = videoCopy.pixels[offset + 2];
+          auxPixelArr[offset + 3] = videoCopy.pixels[offset + 3];
+        }
+        
+        // all the other pixels below the threshold
+        else{
+          let offset2 = (offset + shiftCount) % auxPixelArr.length; 
+          video.pixels[offset] = auxPixelArr[offset];
+          video.pixels[offset + 1] = auxPixelArr[offset + 1];
+          video.pixels[offset + 2] = auxPixelArr[offset + 2];
+          video.pixels[offset + 3] = auxPixelArr[offset + 3];
+        }
+
+        
+      }
     }
   }
+  if (swapModeEnabled) shiftCount += 4;
+  else shiftCount = 0;
+
+  if (assimilate && assimilateCount < 1) assimilateCount += 0.01;
 
   // falsify flag
   chooseIndicesToWarp = false;
- 
+  
   video.updatePixels();
-
-  warpAuxPixelArr();
+  videoPixelsPrev = [];
+ 
+  videoCopy.pixels.forEach(e => videoPixelsPrev.push(e));
+  //warpAuxPixelArr();
   image(video, 0, 0);
 }
 
@@ -88,7 +154,19 @@ function fillIndicesToWarp(){
     for (let x = 0; x < width; x++){
       const index = y * width + x;
       const offset = 4 * index;
-      if (Math.abs(y - mouseY) < 4 ) indicesToWarp.push(index);
+      //if (Math.abs(x - mouseX) < 8 ) indicesToWarp.push(index);
+
+      /*
+      let dx = x - mouseX;
+      let dy = y - mouseY;
+      let radius = 100;
+      if (dx*dx + dy*dy < radius * radius) indicesToWarp.push(index);
+      */
+
+      let dim = 50;
+      let dx = Math.abs(x - mouseX);
+      let dy = Math.abs(y - mouseY);
+      if (dx < dim && dy < dim) indicesToWarp.push(index);
     }
   }
 
@@ -111,9 +189,27 @@ function mouseReleased(){
 function warpAuxPixelArr(){
   indicesToWarp.forEach(function(index, i){
     let offset = 4 * index;
-    auxPixelArr[offset] += 100 * Math.sin(frameCount + i);
-    auxPixelArr[offset + 1] += 80 * Math.cos(frameCount + i);
-    auxPixelArr[offset + 2] -= 200 * Math.cos(frameCount + i * 2);
+    auxPixelArr[offset] += 10 * Math.sin(frameCount + i);
+    auxPixelArr[offset + 1] += 30 * Math.cos(frameCount + i);
+    auxPixelArr[offset + 2] -= 40 * Math.cos(frameCount + i * 2);
   }); 
+}
+
+function keyPressed(){
+  switch(key){
+    case 's':
+      swapModeEnabled = !swapModeEnabled;
+      break;
+    case 'r':
+      // reset
+      indicesNeedingWarp = [];
+      //auxPixelArr = [];
+      indicesToWarp = [];
+      break;
+    case 'a':
+      assimilate = !assimilate;
+      if (!assimilate) assimilateCount = 0;
+      break;
+  }
 }
 
