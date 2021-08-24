fcd54ba86caf4b4b42b0a8ce97f7c2cc36034ef9
kcarollee <kcarollee31693@gmail.com>
2021-08-24 19:57:36 +0900

    more cameras

--- public/practice_sketches/multiple_cameras_practice/main.js
+++ public/practice_sketches/multiple_cameras_practice/main.js
@@ -6,52 +6,64 @@ function main(){
 //CAMERA
 	const cameraArr = [];
 	const fov = 75;
-	const aspect = 2; // display aspect of the canvas
+	const aspect = canvas.clientWidth / canvas.clientHeight; // display aspect of the canvas
 	const near = 0.1;
 	const far = 1000;
 	const camera1 = new THREE.PerspectiveCamera(fov, aspect, near, far);
 	const camera2 = new THREE.PerspectiveCamera(fov, aspect, near, far);
-	camera1.position.set(0, 0, 20);
-	camera2.position.set(0, 0, -20);
-	camera2.position.lookAt = new THREE.Vector3(0, 0, 0);
+	camera1.position.set(0, 20, 30);
+	camera2.position.set(0, 0, 30);
+	// x > 0: rotate cam left
+	// y > 0: rotate cam up
+	// z > 0: 
+	camera2.lookAt(new THREE.Vector3(10, 0, 0)); 
 
 	cameraArr.push(camera1, camera2);
 	const scene = new THREE.Scene();
 	scene.background = new THREE.Color(0xCCCCCC);
-	renderer.render(scene, camera1);
+	
 
 //GEOMETRIES
 	const cubeMat = new THREE.MeshNormalMaterial();
 	const cubeGeom = new THREE.BoxGeometry(10, 10, 10);
-	const cube = new THREE.Mesh(cubeGeom, cubeMat);
-	scene.add(cube);
+	const cubeNum = 1000;
+	for (let i = 0; i < cubeNum; i++){
+		const cube = new THREE.Mesh(cubeGeom, cubeMat);
+		cube.position.set(Math.random() * 10 - 5, Math.random() * 10 - 5, Math.random() * 10 - 5);
+		cube.scale.set(Math.random() * 0.5, Math.random() * 0.5, Math.random() * 0.5);
+		scene.add(cube);
+	}
+	
+	
 
 	
 //GUI
 	const gui = new dat.GUI();
 	const controls = new function(){
-		this.outputObj = function(){
-			scene.children.forEach(c => console.log(c));
+		
+		this.debug = function(){
+
 		}
 	}
 
-	gui.add(controls, 'outputObj');
-
-
+	
+	gui.add(controls, 'debug');
 
 	function render(time){
 		time *= 0.001;
 		step += 1;
+		
 
-		/*
+		//camera2.lookAt(new THREE.Vector3(0, 5 + 5 * Math.sin(step * 0.01), 0))
 		if (resizeRenderToDisplaySize(renderer)){
 			const canvas = renderer.domElement;
-			camera1.aspect = canvas.clientWidth / canvas.clientHeight;
-			camera1.updateProjectionMatrix();
+			camera2.aspect = canvas.clientWidth / canvas.clientHeight;
+			camera2.updateProjectionMatrix();
 		}
-		renderer.render(scene, camera1);
+		renderer.render(scene, camera2);
 		requestAnimationFrame(render);
-		*/
+		
+		/*
 		cameraArr.forEach(function(c, i){
 			if (i == 0){
 				renderer.setViewport(0, 0, 0.5 * window.innerWidth, 1 * window.innerHeight);
@@ -75,6 +87,7 @@ function main(){
 			}
 		});
 		requestAnimationFrame(render);
+		*/
 	}
 
 	function resizeRenderToDisplaySize(renderer){
