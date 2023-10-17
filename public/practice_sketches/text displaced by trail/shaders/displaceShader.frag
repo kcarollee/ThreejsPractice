3ebdfa0090401736be27044c62151aad0633fed2
kcarollee <kcarollee31693@gmail.com>
2023-10-17 12:54:23 +0900

    site revamp

--- public/practice_sketches/text displaced by trail/shaders/displaceShader.frag
+++ public/practice_sketches/text displaced by trail/shaders/displaceShader.frag
@@ -12,18 +12,25 @@ uniform sampler2D dispTargetTex;
 uniform sampler2D dispSourceTex;
 uniform float dispStrength;
 uniform bool flipY;
+uniform bool postProcessOn;
+uniform bool backgroundMode;
 varying vec2 vTexCoord;
 
 // https://gist.github.com/companje/29408948f1e8be54dd5733a74ca49bb9
 float map(float value, float min1, float max1, float min2, float max2) {
     return min2 + (value - min1) * (max2 - min2) / (max1 - min1);
 }
+// https://stackoverflow.com/questions/4200224/random-noise-functions-for-glsl
+float rand(vec2 co){
+    return fract(sin(dot(co, vec2(12.9898, 78.233))) * 43758.5453);
+}
 
 void main(){
     
     vec2 uv = vTexCoord;
+    vec2 uvOrig = vTexCoord;
     if (flipY) uv.y = 1.0 - uv.y;
-   
+    
     // dispSourceTex
     vec4 nd = texture2D(dispSourceTex, uv);
     float dx = nd.r;
@@ -33,8 +40,47 @@ void main(){
     dy = map(dy, .0, 1.0, -dispStrength, dispStrength);
 
     vec2 disp = vec2(dx, dy);
+    // grainy texture
+    if (postProcessOn ||backgroundMode){
+        float random = rand(vec2(uv.x, uv.y)); 
+        uv += vec2(random * 0.01);
+    }
     // dispTargetTex
-    vec4 ng = texture2D(dispTargetTex, uv + disp);
+    vec2 newUV = uv + disp;
+    vec4 ng = texture2D(dispTargetTex, newUV);
 
+    if (backgroundMode) {
+        float gap = 0.4;
+        float random = rand(vec2(uv.x, uv.y)); 
+        newUV += vec2(random * 0.01);
+        if (newUV.x > 1.0 - gap || newUV.x < gap) ng.rgb = vec3(.0);
+    }
+
+    if (postProcessOn){
+        float rr = rand(vec2(uv.x + 13.0, uv.y + 289.0));
+        float rg = rand(vec2(uv.x + 179.0, uv.y + 17.0));
+        float rb = rand(vec2(uv.x + 179.0, uv.y + 17.0));
+        ng.rgb += vec3(rr, rg, rb) * 0.05;
+        if (ng.r < 0.5) discard;
+    }
+
+    if (backgroundMode){
+        ng.rgb = vec3(sin(ng.r * 100.0));
+        ng.rgb = vec3(step(0.5, ng.r));
+        
+        float rr = rand(vec2(uvOrig.x + 13.0, uvOrig.y + 289.0));
+        float rg = rand(vec2(uvOrig.x + 179.0, uvOrig.y + 17.0));
+        float rb = rand(vec2(uvOrig.x + 11.0, uvOrig.y + 97.0));
+        //ng.rgb *= 0.05;
+        
+        //ng.rgb *= 0.5;
+        //if (ng.r < 0.5) discard;
+        ng.rgb += vec3(rr, rg, rb) * 0.25;
+        //ng.rgb += vec3(rr * 0.1);
+        ng.rgb *= 0.1;
+        
+    }
+    
+    
     gl_FragColor = vec4(ng.rgb, 1.0);
 }
--- public/practice_sketches/text displaced by trail/sketch.js
+++ public/practice_sketches/text displaced by trail/sketch.js
@@ -1,22 +1,74 @@
 // noiseGenShader -> generates noise to be displaced
 // noiseDispShader -> generates noise to displace above noise
 // displaceShader -> actually does the displacing
-
-let noiseGenShader, displaceShader, displaceShader2;
-let noiseGenFbo, noiseDispFbo, displaceFbo, displaceTextFbo, textFbo;
+let mainCanvas;
+let noiseGenShader, noiseDispShader, backgroundNoiseShader, displaceShader, displaceShader2;
+let noiseGenFbo, noiseDispFbo, displaceFbo, displaceTextFbo, textFbo, finalFbo, backgroundFbo;
 let sketchWidth, sketchHeight;
-let font;
+let font, mainChar;
+let charPosX, charPosY;
+let displaceImage, noiseGenImage, noiseDispImage, textImage;
+let movableImagesArr = [];
+
 function preload() {
     noiseGenShader = loadShader('shaders/noiseGenShader.vert', 'shaders/noiseGenShader.frag');
     noiseDispShader = loadShader('shaders/noiseDispShader.vert', 'shaders/noiseDispShader.frag');
     displaceShader = loadShader('shaders/displaceShader.vert', 'shaders/displaceShader.frag');
     displaceShader2 = loadShader('shaders/displaceShader.vert', 'shaders/displaceShader.frag');
+    backgroundNoiseShader = loadShader('shaders/displaceShader.vert', 'shaders/displaceShader.frag');
+    font = loadFont("fonts/helvetica.ttf");
 }
 
 
+class MovableImage{
+    constructor(posX, posY, imgWidth, imgHeight, fbo){
+        this.posX = posX;
+        this.posY = posY;
+        this.prevPosX = null;
+        this.prevPosY = null;
+        this.imgWidth = imgWidth;
+        this.imgHeight = imgHeight;
+        this.fbo = fbo;
+        this.moveMode = false;
+    }
+
+    display(){
+        image(this.fbo, this.posX, this.posY, this.imgWidth, this.imgHeight);
+    }
+
+    updatePos(){
+        if (this.moveMode) {
+            this.prevPosX = this.posX;
+            this.posX = mouseX;
+        }
+            
+    }
+
+    mouseIsInside(){
+        if (mouseX < this.posX + this.imgWidth * 0.5 && mouseX > this.posX - this.imgWidth * 0.5){
+            if (mouseY < this.posY + this.imgHeight * 0.5 && mouseY > this.posY - this.imgHeight * 0.5){
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+}
+
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+    mainCanvas = createCanvas(windowWidth, windowHeight);
+    mainCanvas.position(0, 0);
+    mainCanvas.style('z-index', '-1');
+    /*
+    image(displaceTextFbo, width * 0.5 - sketchWidth, height * 0.5, sketchWidth, sketchHeight);
+    image(displaceTextFbo, width * 0.5, height * 0.5, sketchWidth, sketchHeight);
+    image(displaceTextFbo, width * 0.5 + sketchWidth, height * 0.5, sketchWidth, sketchHeight);
+    image(displaceFbo, 200, posYOffset + posYInc, sketchWidth * 0.25, sketchHeight * 0.25);
+    image(noiseGenFbo, 200, posYOffset + posYInc * 2.0, sketchWidth * 0.25, sketchHeight * 0.25);
+    image(noiseDispFbo, 200, posYOffset + posYInc * 3.0, sketchWidth * 0.25, sketchHeight * 0.25);
+    image(textFbo, 200, posYOffset + posYInc * 4.0, sketchWidth * 0.25, sketchHeight * 0.25);
+    */
 
     sketchWidth = min(windowWidth, windowHeight) * 0.75;
     sketchHeight = sketchWidth;
@@ -24,65 +76,153 @@ function setup() {
     noiseDispFbo = createGraphics(sketchWidth, sketchHeight, WEBGL);
     displaceFbo = createGraphics(sketchWidth, sketchHeight, WEBGL);
     displaceTextFbo = createGraphics(sketchWidth, sketchHeight, WEBGL);
+    backgroundFbo = createGraphics(width, height, WEBGL);
     textFbo = createGraphics(sketchWidth, sketchHeight);
 
+    let posYInc = sketchHeight * 0.25;
+    let posYOffset = (height - sketchHeight) * 0.5 - posYInc * 0.5;
+    displaceImage = new MovableImage(random() * windowWidth, posYOffset + posYInc * 1.0, sketchWidth * 0.25, sketchHeight * 0.25, displaceFbo);
+    noiseGenImage = new MovableImage(random() * windowWidth, posYOffset + posYInc * 2.0, sketchWidth * 0.25, sketchHeight * 0.25, noiseGenFbo);
+    noiseDispImage = new MovableImage(random() * windowWidth, posYOffset + posYInc * 3.0, sketchWidth * 0.25, sketchHeight * 0.25, noiseDispFbo);
+    textImage = new MovableImage(random() * windowWidth, posYOffset + posYInc * 4.0, sketchWidth * 0.25, sketchHeight * 0.25, textFbo);
+    
+    movableImagesArr = [
+        displaceImage, noiseGenImage, noiseDispImage, textImage
+    ];
+    textFbo.textFont(font);
     textFbo.textAlign(CENTER);
-    textFbo.textSize(800);
-    textFbo.textFont('Helvetica');
-
+    textFbo.textSize(sketchWidth);
+    //textFbo.textFont('Helvetica');
+    mainChar = String.fromCharCode(int(random(65, 91)));
+    charPosX;
+    charPosY;
     imageMode(CENTER);
 }
 
 function draw() {
     background(0);
+    image(backgroundFbo, width * 0.5, height * 0.5, width, height);
+    smooth();
     // draw text to be distorted
-    textFbo.background(0)
+    textFbo.background(100);
     textFbo.noFill();
     textFbo.stroke(255);
     textFbo.strokeWeight(10);
-    textFbo.text("T", sketchWidth * 0.5, sketchHeight * 0.9);
-    
+    textFbo.textSize(sketchWidth);
+    let bbox = font.textBounds(mainChar, 0, 0, sketchWidth);
+    textFbo.text(mainChar, 0.5 * (sketchWidth), (bbox.h + sketchHeight) * 0.5);
 
+    textImage.display();
+    textImage.updatePos();
+
+    if (textImage.moveMode){
+        if (textImage.prevPosX == textImage.posX){}
+        else{
+            if (frameCount % 5 == 0) mainChar = String.fromCharCode(int(random(65, 91)));
+        }
+        
+    }
+
+    // if noiseGenImage is in moveMode, let the normalized mouseX position value in 
+    // respect of windowWidth be the density controller
+
+    let density = noiseGenImage.posX / windowWidth;
+    density = map(density, 0, 1, 10, 100);
+    
     noiseGenFbo.shader(noiseGenShader);
     noiseGenShader.setUniform('resolution', [sketchWidth, sketchHeight]);
-    noiseGenShader.setUniform('time', frameCount * 0.01);
-    noiseGenShader.setUniform('density', 100.0);
+    noiseGenShader.setUniform('time', frameCount * 0.001);
+    noiseGenShader.setUniform('density', density);
     noiseGenFbo.rect(0, 0, 50, 50);
+
+    noiseGenImage.display();
+    noiseGenImage.updatePos();
+
+    let density2 = noiseDispImage.posX / windowWidth;
+    density2 = map(density2, 0, 1, 1, 5);
 
     noiseDispFbo.shader(noiseDispShader);
     noiseDispShader.setUniform('resolution', [sketchWidth, sketchHeight]);
-    noiseDispShader.setUniform('time', frameCount * 0.01);
-    noiseDispShader.setUniform('density', 2);
+    noiseDispShader.setUniform('time', frameCount * 0.001);
+    noiseDispShader.setUniform('density', density2);
     noiseDispFbo.rect(0, 0, 50, 50);
+
+    noiseDispImage.display();
+    noiseDispImage.updatePos();
+
+    let displaceStrength = displaceImage.posX / windowWidth;
+    displaceStrength = map(displaceStrength, 0, 1, 0, 0.3);
 
     displaceFbo.shader(displaceShader);
     displaceShader.setUniform('resolution', [sketchWidth, sketchHeight]);
-    displaceShader.setUniform('time', frameCount * 0.01);
+    displaceShader.setUniform('time', frameCount * 0.001);
     displaceShader.setUniform('dispTargetTex', noiseGenFbo);
     displaceShader.setUniform('dispSourceTex', noiseDispFbo);
-    displaceShader.setUniform('dispStrength', 0.1);
+    displaceShader.setUniform('dispStrength', displaceStrength);
     displaceFbo.rect(0, 0, 50, 50);
 
+    displaceImage.display();
+    displaceImage.updatePos();
+
+    displaceTextFbo.clear();
     displaceTextFbo.shader(displaceShader2);
     displaceShader2.setUniform('resolution', [sketchWidth, sketchHeight]);
-    displaceShader2.setUniform('time', frameCount * 0.01);
+    displaceShader2.setUniform('time', frameCount * 0.001);
     displaceShader2.setUniform('flipY', true);
+    displaceShader2.setUniform('postProcessOn', true);
     displaceShader2.setUniform('dispTargetTex', textFbo);
     displaceShader2.setUniform('dispSourceTex', displaceFbo);
     displaceShader2.setUniform('dispStrength', 0.05);
-    displaceTextFbo.rect(0, 0, 50, 50);
+    displaceTextFbo.rect(0, 0, 0, 0);
 
-    displaceTextFbo.smooth();
-    //image(displaceFbo, width * 0.5, height * 0.5, width, height);
+    //displaceTextFbo.smooth();
+
+    backgroundFbo.clear();
+    backgroundFbo.shader(backgroundNoiseShader);
+    backgroundNoiseShader.setUniform('resolution', [width, height]);
+    backgroundNoiseShader.setUniform('time', frameCount * 0.001);
+    backgroundNoiseShader.setUniform('flipY', true);
+    //backgroundNoiseShader.setUniform('postProcessOn', true);
+    backgroundNoiseShader.setUniform('backgroundMode', true);
+    backgroundNoiseShader.setUniform('dispTargetTex', noiseGenFbo);
+    backgroundNoiseShader.setUniform('dispSourceTex', noiseDispFbo);
+    backgroundNoiseShader.setUniform('dispStrength', displaceStrength);
+    backgroundFbo.rect(0, 0, 50, 50);
+
+    //image(backgroundFbo, width * 0.5, height * 0.5, width, height);
+
+    let posYInc = sketchHeight * 0.25;
+    let posYOffset = (height - sketchHeight) * 0.5 - posYInc * 0.5;
+    
+    //image(displaceTextFbo, width * 0.5 - sketchWidth, height * 0.5, sketchWidth, sketchHeight);
     image(displaceTextFbo, width * 0.5, height * 0.5, sketchWidth, sketchHeight);
-    image(displaceFbo, 400, 400, sketchWidth * 0.25, sketchHeight * 0.25);
-    image(noiseGenFbo, 200, 200, sketchWidth * 0.25, sketchHeight * 0.25);
-    image(noiseDispFbo, 600, 600, sketchWidth * 0.25, sketchHeight * 0.25);
-    image(textFbo, 800, 800, sketchWidth * 0.25, sketchHeight * 0.25);
+    //image(displaceTextFbo, width * 0.5 + sketchWidth, height * 0.5, sketchWidth, sketchHeight);
+    
 }
 
 function windowResized(){
     resizeCanvas(windowWidth, windowHeight)
     sketchWidth = min(windowWidth, windowHeight) * 0.75;
     sketchHeight = sketchWidth;
+    //textFbo.textSize(sketchWidth);
+
+    movableImagesArr.forEach(function(img){
+        
+    });
+}
+
+function mousePressed(){
+    movableImagesArr.forEach(function(img){
+        if (img.mouseIsInside()){
+            img.moveMode = true;
+        }
+    });
+}
+
+function mouseReleased(){
+    movableImagesArr.forEach(function(img){
+        if (img.mouseIsInside()){
+            img.moveMode = false;
+        }
+    });
 }
